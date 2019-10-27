b1054fbc76c799a2ef9067d79e56778a506db6c1
Vyacheslav <myeve2007@yandex.ru>
2019-10-27 09:42:34 +0300

    feat: add data into main script event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,28 @@
 window.onload = function () {
     (function () {
         let getJsonData = function (url) {
-            return fetch(url).then(function(value) {
-                console.log(value.json());
-            }, function(reason) {
-                console.log(reason);
+            return fetch(url).then(function (value) {
+                return value.json();
+            }, function (reason) {
+                return console.log(reason);
             });
         };
 
         document.querySelectorAll('.work-area-right-panel-block-sizes__item').forEach(function (item) {
             item.addEventListener('click', function (e) {
                 e.preventDefault();
-                
-                let data = this.dataset.data;
-                console.log(data);
+
+                let url = this.dataset.url;
+                let dataType = this.dataset.type;
+                const canvas = document.querySelector('.work-area-canvas-block__canvas');
+                const ctx = canvas.getContext('2d');
+
+                if (dataType === 'json') {
+                    getJsonData(url)
+                        .then(function (data) {
+                            console.log(data);
+                        });
+                }
             })
         });
     })();
